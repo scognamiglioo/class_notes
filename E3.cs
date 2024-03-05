9c42488bfb45f87ee1df0b992c51fb82e55ebccc
julia <85513215+scognamiglioo@users.noreply.github.com>
2024-03-05 10:37:44 -0300

    up

--- E3.cs
+++ E3.cs
@@ -33,20 +33,20 @@ class Program
         }
 
 // lambda
-    Console.Write("Digite o primeiro número: ");
+        Console.Write("Digite o primeiro número: ");
         int num1 = Convert.ToInt32(Console.ReadLine());
 
         Console.Write("Digite o segundo número: ");
         int num2 = Convert.ToInt32(Console.ReadLine());
 
-        Func<int, int, int> operacoes = (a, b) =>
-        {
-            Console.WriteLine($"Adição: {a + b}");
-            Console.WriteLine($"Subtração: {a - b}");
-            Console.WriteLine($"Multiplicação: {a * b}");
-            Console.WriteLine($"Divisão inteira: {a / b}");
-            Console.WriteLine($"Resto da divisão: {a % b}");
-            return 0;
-        };
+        Func<int, int, (int ad, int sub, int mult, int div,int rest)> all =
+            (x, y) => ((x + y), (x - y), (x * y), (x / y),(x%y));
+        
+        var result = all(num1, num2); 
+        Console.WriteLine($"Soma: {result.ad}");
+        Console.WriteLine($"Subtração: {result.sub}");
+        Console.WriteLine($"Multiplicação: {result.mult}");
+        Console.WriteLine($"Divisão: {result.div}");
+        Console.WriteLine($"Resto: {result.rest}");
 
 
